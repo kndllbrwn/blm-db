c6fe0746ce785adfb08d67c3c7244354623b5ac6
Kendall Brown <kndllbrwn1914@gmail.com>
2020-11-03 21:47:37 -0500

    Updating config to allow test access to api

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,15 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
 module.exports = {
     plugins: [
-        {
-            resolve: "gatsby-source-graphql",
-            options: {
-                // This type will contain the remote schema Query type
-                typeName: "AWSAppSync",
-                // This is the field under which it's accessible
-                fieldName: "Rose",
-                // URL to query from
-                url: "https://2apnrm7ymreqfkjznhzs45nxu4.appsync-api.us-east-1.amazonaws.com/graphql",
-                headers: {
-                    "x-api-key": "da2-kk5hi7xxpfcfrfiki4lddfroni"
-                },
-                refetchInterval: 10,
-            },
+      {
+        resolve: "gatsby-source-graphql",
+        options: {
+          typeName: "blmDB",
+          fieldName: "rose",
+          url: "https://2apnrm7ymreqfkjznhzs45nxu4.appsync-api.us-east-1.amazonaws.com/graphql",
+          headers: {
+            "x-api-key": "da2-kk5hi7xxpfcfrfiki4lddfroni"
+          },
         },
+      },
     ],
   }
-  
